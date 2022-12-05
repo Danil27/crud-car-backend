654b5cae6c2da115ca96140a4b87fb112134d69e
Krygin Danila <danil27dzr@gmail.com>
2022-12-05 13:49:28 +0300

    fix: clear project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CarModule } from './car/car.model';
 import { configOptions } from './config';
 import { DatasourceModule } from './datasourse/datasourse.module';
 
 @Module({
   imports: [ConfigModule.forRoot(configOptions), DatasourceModule, CarModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
